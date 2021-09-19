4427f2bac543a8cca5ab7e999db741f07b61233c
Vimal Raveendran <vimalraveendra@gmail.com>
2021-09-19 16:31:56 +0200

    initial commit

---
+++ DependencyInjection/Model/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DependencyInjection.Model
+{
+    public interface IRepository
+    {
+        IEnumerable<Product> Products { get; }
+
+        Product this[string name] { get; }
+
+        void AddProduct(Product product);
+
+        void DeleteProduct(Product product);
+    }
+}
